c6862370742f2b69238a3f8d6ebcb2b06e8baf52
Chandan-Raj <chandanraj1919@gmail.com>
2016-11-24 16:03:28 +0530

    Core Java Basics
    Added parameterised constructors

---
+++ CoreJavaBasics/src/com/constructors/ParaC.java
@@ -0,0 +1,17 @@
+package com.constructors;
+
+public class ParaC 
+{
+ int id;
+ String name;
+ 
+ ParaC(int i,String n)
+ {
+	 id = i;
+	 name =n;
+ }
+ void display()
+ {
+	 System.out.println(id+" "+name);
+ }
+}
